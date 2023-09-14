a2c082eb19ed63069f76d40dba27a799835c8193
+ <+>
2023-09-14 16:21:51 -0600

    finished lab

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -14,6 +14,10 @@ class Student:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
+    def course_count(self):
+        return len(self._enrollments)
+    
 
 class Course:
     def __init__(self, title):
@@ -29,7 +33,13 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
-
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
 
 class Enrollment:
     all = []
